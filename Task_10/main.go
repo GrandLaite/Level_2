3344c83a7221b422f9adae924a8b51e2caafd1ce
GrandLaite <danil12345989@gmail.com>
2025-01-23 09:32:16 +0600

    Update

--- Task_10/main.go
+++ Task_10/main.go
@@ -1,3 +1,69 @@
-/*
-Реализовать утилиту wget с возможностью скачивать сайты целиком.
-*/
+/* Реализовать утилиту wget с возможностью скачивать сайты целиком. */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	var outputDir string
+	flag.StringVar(&outputDir, "output", ".", "директория для сохранения страницы")
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		fmt.Println("Usage: wget [--output=directory] URL")
+		os.Exit(1)
+	}
+
+	startURL := flag.Arg(0)
+
+	err := downloadMainPage(startURL, outputDir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Ошибка загрузки страницы: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Страница успешно загружена!")
+}
+
+func downloadMainPage(pageURL, outputDir string) error {
+	resp, err := http.Get(pageURL)
+	if err != nil {
+		return fmt.Errorf("ошибка подключения к %s: %v", pageURL, err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("не удалось загрузить %s: статус %d", pageURL, resp.StatusCode)
+	}
+
+	u, err := url.Parse(pageURL)
+	if err != nil {
+		return fmt.Errorf("не удалось разобрать URL %s: %v", pageURL, err)
+	}
+	fileName := filepath.Join(outputDir, filepath.Base(u.Path))
+	if fileName == outputDir || filepath.Ext(fileName) == "" {
+		fileName = filepath.Join(outputDir, "index.html")
+	}
+
+	f, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("ошибка создания файла %s: %v", fileName, err)
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		return fmt.Errorf("ошибка записи в файл %s: %v", fileName, err)
+	}
+
+	fmt.Printf("Страница загружена: %s -> %s\n", pageURL, fileName)
+	return nil
+}
--- Task_11/main.go
+++ Task_11/main.go
@@ -14,3 +14,62 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 Если сокет закрывается со стороны сервера, программа должна также завершаться.
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	var timeout string
+	flag.StringVar(&timeout, "timeout", "10s", "таймаут на подключение (например, 5s, 1m)")
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		fmt.Println("Usage: go-telnet [--timeout=10s] host port")
+		os.Exit(1)
+	}
+
+	host := flag.Arg(0)
+	port := flag.Arg(1)
+
+	duration, err := time.ParseDuration(timeout)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Ошибка: неверный формат таймаута: %v\n", err)
+		os.Exit(1)
+	}
+
+	address := net.JoinHostPort(host, port)
+	conn, err := net.DialTimeout("tcp", address, duration)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Ошибка подключения: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Подключено к %s\n", address)
+	defer conn.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		if _, err := io.Copy(os.Stdout, conn); err != nil {
+			fmt.Fprintf(os.Stderr, "Соединение закрыто сервером: %v\n", err)
+		}
+		done <- struct{}{}
+	}()
+
+	go func() {
+		if _, err := io.Copy(conn, os.Stdin); err != nil {
+			fmt.Fprintf(os.Stderr, "Ошибка записи в сокет: %v\n", err)
+		}
+		done <- struct{}{}
+	}()
+
+	<-done
+	fmt.Println("\nСоединение закрыто. Завершение программы.")
+}
